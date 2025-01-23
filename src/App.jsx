962b209149b5fbe6dcb3a2f002547c35ac54d2e0
RKOKA209 <48864636+RKOKA209@users.noreply.github.com>
2025-01-23 01:34:38 -0500

    Added new courses

--- src/App.jsx
+++ src/App.jsx
@@ -307,7 +307,10 @@ const Portfolio = () => {
                         "Systems Programming",
                         "Software Testing",
                         "Competitive Programming",
-                        "Object Oriented Programming"
+                        "Object Oriented Programming",
+                        "Data Mining & Machine Learning",
+                        "Information Systems",
+                        "Senior Project"
                       ].map((course, i) => (
                         <span key={i} 
                               className="px-3 py-1 bg-blue-100 dark:bg-blue-900 text-blue-800 dark:text-blue-200 rounded-full text-sm">
